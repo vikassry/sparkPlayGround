f62da722020edda2291d3394488f76952acbe98a
vikassry <vikassry@gmail.com>
2016-08-03 19:30:14 +0530

    setup working

--- src/java/com/tw/samples/JavaWordCount.java
+++ src/java/com/tw/samples/JavaWordCount.java
@@ -1,15 +1,20 @@
 package com.tw.samples;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.PairFunction;
 import scala.Tuple2;
 
 import java.util.Arrays;
+import java.util.Date;
+import java.util.Iterator;
 import java.util.regex.Pattern;
 
 public final class JavaWordCount {
@@ -21,21 +26,29 @@ public final class JavaWordCount {
             System.exit(1);
         }
 
-        SparkConf sparkConf = new SparkConf().setAppName("JavaWordCount");
+        SparkConf sparkConf = new SparkConf().setAppName("JavaWordCount").setMaster("local");
+        Logger.getLogger("org").setLevel(Level.OFF);
+        Logger.getLogger("akka").setLevel(Level.OFF);
         JavaSparkContext ctx = new JavaSparkContext(sparkConf);
-        JavaRDD<String> lines = ctx.textFile(args[0], 1);
+        JavaRDD<String> logData = ctx.textFile(args[0], 1).cache();
 
-        JavaPairRDD<String, Integer> counts = wordCount(lines);
+        long numAs = logData.filter(new Function<String, Boolean>() {
+            public Boolean call(String s) { return s.contains("a"); }
+        }).count();
 
-        counts.saveAsTextFile("output");
-        ctx.stop();
+        long numBs = logData.filter(new Function<String, Boolean>() {
+            public Boolean call(String s) { return s.contains("b"); }
+        }).count();
+
+        System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
+
     }
 
     public static JavaPairRDD<String, Integer> wordCount(JavaRDD<String> lines) {
         JavaRDD<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
             @Override
-            public Iterable<String> call(String string) {
-                return Arrays.asList(SPACE.split(string));
+            public Iterator<String> call(String s) throws Exception {
+                return Arrays.asList(SPACE.split(s)).iterator();
             }
         });
 
--- src/scala/com/tw/samples/ScalaWordCount.scala
+++ src/scala/com/tw/samples/ScalaWordCount.scala
@@ -9,18 +9,17 @@ object ScalaWordCount{
             println("Usage: ScalaWordCount <file>")
             System.exit(1)
         }
-        val inputFile:String = args(0)
-        val sparkConf: SparkConf = new SparkConf().setAppName("JavaWordCount")
+        val logFile:String = args(0)
+        val sparkConf: SparkConf = new SparkConf().setAppName("JavaWordCount").setMaster("local")
         val sc: SparkContext = new SparkContext(sparkConf)
-        val textFile = sc.textFile(inputFile)
-        val counts: RDD[(String, Int)] = wordCount(textFile)
-        counts.saveAsTextFile("output")
+        val logData = sc.textFile(logFile, 2).repartition(2).cache()
+        wordCount(logData)
     }
 
-    def wordCount(textFile: RDD[String]): RDD[(String, Int)] = {
-        val counts = textFile.flatMap(line => line.split(" "))
-            .map(word => (word, 1))
-            .reduceByKey(_ + _)
-        counts
+    def wordCount(textFile : RDD[String]): Unit ={
+        val wordCounts: RDD[(String, Int)] = textFile.flatMap(_.split(" ").map(word => {(word, 1)})).reduceByKey((a, b)=>{a+b})
+        val counts: Array[(String, Int)] = wordCounts.collect()
+        println("All Word Counts :")
+        counts.foreach(tuple=>{println(s"${tuple._1}:${tuple._2}")})
     }
 }
